c8461f7b7c95d759234d61f2684ba8cb293e27f2
TamaRe876 <45084726+TamaRe876@users.noreply.github.com>
2022-07-23 18:05:26 -0500

    Update Auth.jsx
    changed url to server on netifly

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -28,7 +28,7 @@ const Auth = () => {
 
         const { username, password, phoneNumber, avatarURL } = form;
 
-        const URL = 'https://technetjachat.netlify.app';
+        const URL = 'https://deskhelper.netlify.app';
       
 
         const { data: { token, userId, hashedPassword, fullName } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
